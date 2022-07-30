409b2021d749cde29e31ac186fd1ac10a4125ad7
Samuel Oyelami <oyelamisamuel31@gmail.com>
2022-07-30 22:33:06 +0100

    initial with axios, tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app.js",
+    "./src/index.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
